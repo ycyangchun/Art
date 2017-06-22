c1d3b69a09148e20dde8671237993aec710336b4
ycyangchun <rujishui@163.com>
2017-06-22 18:06:15 +0800

    倒计时

--- app/src/main/java/com/funs/appreciate/art/view/SplashActivity.java
+++ app/src/main/java/com/funs/appreciate/art/view/SplashActivity.java
@@ -2,9 +2,12 @@ package com.funs.appreciate.art.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.funs.appreciate.art.ArtApp;
+import com.funs.appreciate.art.R;
 import com.funs.appreciate.art.base.BaseActivity;
 import com.funs.appreciate.art.di.components.DaggerSplashComponent;
 import com.funs.appreciate.art.di.modules.SplashModule;
@@ -22,10 +25,13 @@ public class SplashActivity extends BaseActivity implements SplashContract.View 
 
     @Inject
     SplashPresenter presenter;
+
+    TextView count_down_tv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash);
+        count_down_tv = (TextView) findViewById(R.id.count_down_tv);
         DaggerSplashComponent.builder()
                 .netComponent(ArtApp.get(this).getNetComponent())
                 .splashModule(new SplashModule(this))
@@ -47,8 +53,20 @@ public class SplashActivity extends BaseActivity implements SplashContract.View 
 
     @Override
     public void loadSplashSuccess(String splash) {
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        CountDownTimer countDownTimer = new CountDownTimer(5 * 1000 ,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                count_down_tv.setText( "时间剩余"+millisUntilFinished / 1000 +"\"");
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+        countDownTimer.start();
+
     }
 
     @Override
