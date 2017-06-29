60804da6b19b10ad9ba97988b20ad1af6ff7b890
ycyangchun <rujishui@163.com>
2017-06-29 14:23:51 +0800

    图片资源接口 1

--- app/src/main/java/com/funs/appreciate/art/view/MainActivity.java
+++ app/src/main/java/com/funs/appreciate/art/view/MainActivity.java
@@ -1,6 +1,5 @@
 package com.funs.appreciate.art.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -8,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -28,13 +26,11 @@ import com.funs.appreciate.art.model.entitys.LayoutModel;
 import com.funs.appreciate.art.model.entitys.PictureModel;
 import com.funs.appreciate.art.presenter.MainContract;
 import com.funs.appreciate.art.presenter.MainPresenter;
-import com.funs.appreciate.art.service.ScreenProtectionService;
 import com.funs.appreciate.art.utils.MsgHelper;
 import com.funs.appreciate.art.view.widget.TabFocusRelative;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
