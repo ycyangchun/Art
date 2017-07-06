902ae4051f5f25f0c88708eb8896f183fca196e6
ycyangchun <rujishui@163.com>
2017-07-06 15:56:02 +0800

    decoder->decode returned false

--- app/src/main/java/com/funs/appreciate/art/utils/UtilTimer.java
+++ app/src/main/java/com/funs/appreciate/art/utils/UtilTimer.java
@@ -26,6 +26,8 @@ public class UtilTimer {
         this.mContext = context.getApplicationContext();//单例的生命周期和应用的一样长，这样就防止了内存泄漏。
         this.mHandler = handle;
         times = ArtResourceUtils.getScreenSaverTime(30) * 60 * 1000;
+//        times = ArtResourceUtils.getScreenSaverTime(5)  * 1000;
+
     }
 
     // 提供一个全局的静态方法
--- app/src/main/java/com/funs/appreciate/art/view/ScreenProtectionActivity.java
+++ app/src/main/java/com/funs/appreciate/art/view/ScreenProtectionActivity.java
@@ -14,6 +14,9 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.funs.appreciate.art.ArtApp;
 import com.funs.appreciate.art.R;
 import com.funs.appreciate.art.di.components.DaggerScreenProtectionComponent;
@@ -154,8 +157,21 @@ public class ScreenProtectionActivity extends FragmentActivity implements Splash
 //        System.out.println("======== url =========>"+url+" picIndex "+picIndex);
         Glide.with(instance)
                 .load(url)
+                .override(1980, 1080)
+//                .decoder()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .thumbnail(0.5f)
+                .thumbnail(0.2f)
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        return false;
+                    }
+                })
                 .error(R.drawable.bg_splash)
                 .into(splash_iv);
     }
