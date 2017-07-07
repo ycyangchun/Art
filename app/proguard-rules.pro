d1e29b7ec6e70de270a313b3493546a5b72a26f5
ycyangchun <rujishui@163.com>
2017-07-07 14:33:17 +0800

    talkingData

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -23,3 +23,15 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+-dontwarn com.tendcloud.tenddata.**
+-keep class com.tendcloud.** {*;}
+-keep public class com.tendcloud.tenddata.** { public protected *;}
+-keepclassmembers class com.tendcloud.tenddata.**{
+public void *(***);
+}
+-keep class com.talkingdata.sdk.TalkingDataSDK {public *;}
+-keep class com.apptalkingdata.** {*;}
+-keep class dice.** {*; }
+-dontwarn dice.**
+
--- app/src/main/java/com/funs/appreciate/art/ArtApp.java
+++ app/src/main/java/com/funs/appreciate/art/ArtApp.java
@@ -8,6 +8,7 @@ import com.funs.appreciate.art.di.components.NetComponent;
 import com.funs.appreciate.art.di.modules.NetModule;
 import com.funs.appreciate.art.utils.PathUtils;
 import com.funs.appreciate.art.utils.SharedPreferencesUtils;
+import com.tendcloud.tenddata.TCAgent;
 import com.umeng.analytics.MobclickAgent;
 
 /**
@@ -30,6 +31,15 @@ public class ArtApp  extends Application{
         initNet();
         initPath();
         initDadaBase();
+
+        // talkingData
+        TCAgent.LOG_ON = ArtConfig.IS_DEBUG;
+        // App ID: 在TalkingData创建应用后，进入数据报表页中，在“系统设置”-“编辑应用”页面里查看App ID。
+        // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
+//        TCAgent.init(this, "您的 App ID", "渠道 ID");
+        // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
+        TCAgent.init(this);
+        TCAgent.setReportUncaughtExceptions(true);
     }
 
     //path
