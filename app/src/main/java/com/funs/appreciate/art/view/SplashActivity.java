2cf2975a22932fafb74a632e299281d7421f43ef
ycyangchun <rujishui@163.com>
2017-07-12 16:11:50 +0800

    图片加载完 倒计时

--- app/src/main/java/com/funs/appreciate/art/view/SplashActivity.java
+++ app/src/main/java/com/funs/appreciate/art/view/SplashActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.funs.appreciate.art.ArtApp;
 import com.funs.appreciate.art.R;
 import com.funs.appreciate.art.base.BaseActivity;
@@ -37,6 +40,7 @@ public class SplashActivity extends BaseActivity implements SplashContract.View 
 
     TextView count_down_tv;
     ImageView splash_iv;
+    CountDownTimer countDownTimer;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,23 +96,13 @@ public class SplashActivity extends BaseActivity implements SplashContract.View 
         SplashPictureEntity.ConfigBean  cb = se.getConfig();
         String picUrl = cb.getDataJson();
 
-        if(picUrl.contains(";")){
-
-        } else {
-            Glide.with(this).load(picUrl)
-                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
-                    .thumbnail(0.2f)
-                    .error(R.drawable.bg_err).into(splash_iv);
-        }
-
         int duration = 5;//默认
         try {
             duration = Integer.parseInt(cb.getDuration());
         } catch (NumberFormatException e) {
             e.printStackTrace();
         } finally {
-
-            CountDownTimer countDownTimer = new CountDownTimer( duration * 1000 ,1000){
+            countDownTimer = new CountDownTimer( duration * 1000 ,1000){
                 @Override
                 public void onTick(long millisUntilFinished) {
                     count_down_tv.setText( "时间剩余"+millisUntilFinished / 1000 +"\"");
@@ -121,7 +115,23 @@ public class SplashActivity extends BaseActivity implements SplashContract.View 
                     finish();
                 }
             };
-            countDownTimer.start();
+            Glide.with(this).load(picUrl)
+                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                    .thumbnail(0.2f)
+                    .error(R.drawable.bg_err)
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            countDownTimer.start();
+                            return false;
+                        }
+                    })
+                    .into(splash_iv);
         }
     }
 
